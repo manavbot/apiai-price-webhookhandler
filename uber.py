3fae13a227f95899508326ed5437d3e229fbedc2
HsHack <hshack@e1z3r7p8.42.us.org>
2017-03-26 03:47:53 -0700

    Transition to demigodism

--- uber.py
+++ uber.py
@@ -1,5 +1,7 @@
 import urllib2
+import json
 from uber_rides.session import Session
+
 
 def Uber(data):
 	request_headers = {
@@ -9,19 +11,17 @@ def Uber(data):
 	session = Session(server_token = "sVG0_JuX6mRTU1FUTvL8wT1M8-2V51wrLv4sKzxaS")
 
 	request = urllib2.Request("https://api.uber.com/v1.2/estimates/price?start_latitude=37.7752315&start_longitude=-122.418075&end_latitude=37.7752415&end_longitude=-122.518075", headers = request_headers)
-	content = urllib2.urlopen(request).read()
+	content = urllib2.urlopen(request).read()	
+	jsoncontent = json.loads(content)
+	print(jsoncontent)
 
-	prices = content.get('prices')
-    if prices is None:
-        return {}
+	prices = jsoncontent['prices']
+	if prices is None:
+		return {}
 
-    localized_display_name = prices[1].get('localized_display_name')
-    if localized_display_name is None:
-        return {}
+	estimate = prices[1]['estimate']
+	if estimate is None:
+		return {}
 
-    estimate = localized_display_name.get('estimate')
-    if estimate is None:
-        return {}
-
-	print(estimate)
-	return estimate	
+	print("Estimate", estimate)
+	return estimate
